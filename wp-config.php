ba58ce437c5f6355c45600aead09e3001c079c25
ryan <loulaner@gmail.com>
2014-03-01 17:33:06 +0800

    update

--- wp-config.php
+++ wp-config.php
@@ -1,9 +1,11 @@
+<?php
 /**
  *  * 开发者专用：WordPress调试模式。
  *   *
  *    * 将这个值改为true，WordPress将显示所有用于开发的提示。
  *     * 强烈建议插件开发者在开发环境中启用WP_DEBUG。
- *      */
+ *      
+ */
 define('WP_DEBUG', false);
 
 /**
@@ -11,7 +13,8 @@ define('WP_DEBUG', false);
  *   *
  *    * 可在设置→常规中修改。
  *     * 如需禁用，请移除或注释掉本行。
- *      */
+ *      
+ */
 define('WP_ZH_CN_ICP_NUM', true);
 
 /* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
